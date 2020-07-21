c0d1067aebec19261359ffa890b9a0aae29186fe
yangzhiqiong <yangzhiqiong@SZ13410_06>
2020-07-21 18:25:17 +0800

    上传文件

---
+++ BlazorApp1/BlazorApp1/PageModel/FileUploader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+//namespace BlazorApp1.PageModel
+//{
+//    public class FileUploader : ComponentBase, IDisposable 
+//    {
+//        private readonly IJSRuntime jsRuntime;
+
+//    }
+//}
--- BlazorApp1/BlazorApp1/PageModel/InputFileModel.cs
+++ BlazorApp1/BlazorApp1/PageModel/InputFileModel.cs
@@ -16,58 +16,23 @@ using System.IO;
 
 namespace BlazorApp1.PageModel
 {
-    public class InputFileModel : ComponentBase, IDisposable
+    public class InputFileModel : ComponentBase
     {
         private IJSRuntime JSRuntime { get; }
         [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> UnmatchedParameters { get; set; }
-        [Parameter] public EventCallback<IFileListEntry[]> OnChange { get; set; }
+        //[Parameter] public EventCallback<IFileListEntry[]> OnChange { get; set; }
         [Parameter] public int MaxMessageSize { get; set; } = 20 * 1024; // TODO: Use SignalR default
         [Parameter] public int MaxBufferSize { get; set; } = 1024 * 1024;
 
         public ElementReference inputFileElement;
         IDisposable thisReference;
 
-        [JSInvokable]
-        public Task NotifyChange(FileListEntryImpl[] files)
+              
+        public async void Upload()
         {
-            foreach (var file in files)
-            {
-                // So that method invocations on the file can be dispatched back here
-                file.Owner = (InputFile)(object)this;
-            }
+            string result = string.Empty;
+            await Task.Run(() => result = JSRuntime.InvokeAsync<string>("getFileText", "#inputFile").Result);
 
-            return OnChange.InvokeAsync(files);
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                thisReference = DotNetObjectReference.Create(this);
-                await JSRuntime.InvokeAsync<object>("BlazorInputFile.init", inputFileElement, thisReference);
-            }
-        }
-
-        internal Stream OpenFileStream(FileListEntryImpl file)
-        {
-            return SharedMemoryFileListEntryStream.IsSupported(JSRuntime)
-                ? (Stream)new SharedMemoryFileListEntryStream(JSRuntime, inputFileElement, file)
-                : new RemoteFileListEntryStream(JSRuntime, inputFileElement, file, MaxMessageSize, MaxBufferSize);
-        }
-
-        internal async Task<FileListEntryImpl> ConvertToImageFileAsync(FileListEntryImpl file, string format, int maxWidth, int maxHeight)
-        {
-            var imageFile = await JSRuntime.InvokeAsync<FileListEntryImpl>("BlazorInputFile.toImageFile", inputFileElement, file.Id, format, maxWidth, maxHeight);
-
-            // So that method invocations on the file can be dispatched back here
-            imageFile.Owner = (InputFile)(object)this;
-
-            return imageFile;
-        }
-
-        void IDisposable.Dispose()
-        {
-            thisReference?.Dispose();
         }
 
     }
